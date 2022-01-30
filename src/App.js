e6b7797b0ba7bd10339b7553fb99246a5c03e628
abdullaev2oo2 <sarvar.bek.790@gmail.com>
2022-01-30 13:59:52 +0500

    first ommit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Die from './Die'
 
-import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
+import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
 
 export default function App() {
